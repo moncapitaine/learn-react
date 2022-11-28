6269d69159f4f4efbd2e9de0fcf5a70de2fbe477
Michael Vogt <michael.vogt@progwise.net>
2022-11-28 16:46:01 +0100

    added new item text field

--- my-react-app/src/pages/TodoPage.tsx
+++ my-react-app/src/pages/TodoPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { ReactSVG, useState } from "react"
 import { MyToDoItem } from "../components/myToDoItem/MyToDoItem"
 import { MyToDoList } from "../components/myToDoList/MyToDoList"
 import { ToDoItem } from "../domain/todoItem"
@@ -22,13 +22,14 @@ const testTodoList: ToDoItem[] = [
 export const TodoPage = () => {
   const [itemList, setItemList] = useState(testTodoList)
   const [selectedItem, setSelectedItem] = useState<ToDoItem>()
+  const [newItemText, setNewItemText] = useState('new item')
 
   const handleSelectItem = (item: ToDoItem) => {
     setSelectedItem(item)
   }
 
   const handleClickNewItem = () => {
-    itemList.push({id: `${itemList.length + 1}`, name: 'Neues item'})
+    itemList.push({id: `${itemList.length + 1}`, name: newItemText})
     setItemList([...itemList])
   }
 
@@ -36,9 +37,15 @@ export const TodoPage = () => {
     const filteredItems = itemList.filter((item) => item.id !== itemToRemove.id)
     setItemList(filteredItems)
   }
+
+  const handleTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setNewItemText(e.target.value)
+  }
+
   return (
     <>
       <h2>Todo Page</h2>
+      <input type="text" onChange={handleTextChange} />
       <button onClick={handleClickNewItem}>Add item</button>
       <MyToDoList listItems={itemList} onSelectItem={handleSelectItem} />
       { selectedItem ? (<MyToDoItem onDeleteItem={handleDeleteItem} item={selectedItem} />) : (<span>Please select an Item...</span>) }
