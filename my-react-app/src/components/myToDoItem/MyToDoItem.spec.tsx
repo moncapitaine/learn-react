544bbd7463177307e48b17391bd593228c029629
Michael Vogt <michael.vogt@progwise.net>
2022-11-30 11:50:17 +0100

    first test using vitest

---
+++ my-react-app/src/components/myToDoItem/MyToDoItem.spec.tsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from 'vitest';
+
+describe('the MyToDoItem', () => {
+  it('renders a form...', () => {
+    console.log('check test')
+    expect(1).toBeTruthy()
+  })
+})
