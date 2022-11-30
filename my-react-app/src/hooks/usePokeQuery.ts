686f9931b17e5b08cbe5aea6ba8279e18668372a
Michael Vogt <michael.vogt@progwise.net>
2022-11-30 09:02:05 +0100

    added custom query hook

---
+++ my-react-app/src/hooks/usePokeQuery.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react"
+
+export interface Pokemon {
+  name: string
+  url: string
+}
+
+export interface PokemonQueryResult {
+  pokemonList: Pokemon[]
+  error: string | undefined
+  loading: boolean
+}
+
+const wait = async (timeInMilliseconds: number): Promise<void> => {
+  const result = new Promise<void>((resolve, _reject) => {
+    setTimeout(() => resolve(undefined), timeInMilliseconds)
+  })
+  return result
+}
+
+const fetchPokemon = async (): Promise<Pokemon[]> => {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
+  const data = await response.json()
+  return data.results
+}
+
+export const usePokeQuery = (): PokemonQueryResult => {
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([])
+  const [error, setError] = useState<string | undefined>()
+  const [loading, setLoading] = useState(false)
+  const loadPokemons = async () => {
+    try {
+      setLoading(true)
+      await wait(4000) // will be awaited
+      // wait(5000) // will not be awaited for
+      const fetchedItems = await fetchPokemon()
+      setPokemonList(fetchedItems)
+      setError(undefined)
+      setLoading(false)
+    }
+    catch(error) {
+      setError('Error fetching pokemons from async')
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    loadPokemons()
+  }, [])
+  return ({
+    pokemonList,
+    loading,
+    error
+  })
+}
--- my-react-app/src/pages/PokemonPage.tsx
+++ my-react-app/src/pages/PokemonPage.tsx
@@ -1,49 +1,9 @@
 import { useEffect, useState } from "react"
-
-export interface Pokemon {
-  name: string
-  url: string
-}
-
-const fetchPokemon = async (): Promise<Pokemon[]> => {
-  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0')
-  const data = await response.json()
-  return data.results
-}
-
-const wait = async (timeInMilliseconds: number): Promise<void> => {
-  const result = new Promise<void>((resolve, _reject) => {
-    setTimeout(() => resolve(undefined), timeInMilliseconds)
-  })
-  return result
-}
+import { usePokeQuery } from "../hooks/usePokeQuery"
 
 export const PokemonPage = () => {
-  const [pokemonList, setPokemonList] = useState<Pokemon[]>([])
-  const [error, setError] = useState<string | undefined>()
-  const [loading, setLoading] = useState(false)
-  const loadPokemons = async () => {
-    try {
-      setLoading(true)
-      await wait(4000) // will be awaited
-      // wait(5000) // will not be awaited for
-      const fetchedItems = await fetchPokemon()
-      setPokemonList(fetchedItems)
-      setError(undefined)
-      setLoading(false)
-    }
-    catch(error) {
-      setError('Error fetching pokemons from async')
-      setLoading(false)
-    }
-  }
 
-  useEffect(() => {
-    // fetchPokemon().then((list) => setPokemonList(list))
-    //   .catch((reason) => setError('Error fetching pokemons from effect')
-    //   )
-    loadPokemons()
-  }, [])
+  const { loading, error, pokemonList } = usePokeQuery()
 
   if (loading) {
     return <div>Loading...</div>
