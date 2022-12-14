dbeb617de6c36c046c3a32d773392d4cc803dc0c
Michael Vogt <michael.vogt@progwise.net>
2022-12-14 11:47:22 +0100

    added routes

--- telekom-learn-react/src/App.tsx
+++ telekom-learn-react/src/App.tsx
@@ -1,7 +1,27 @@
+import { Route, Routes } from 'react-router'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { Banner } from './components/Banner'
+import { MainNavigation } from './components/MainNavigation'
+import { HomePage } from './pages/HomePage'
+import { NewRecipePage } from './pages/NewRecipePage'
+import { NotFoundPage } from './pages/NotFoundPage'
 
 export const App = ():JSX.Element => {
   return (
-    <div className='app'>Hi Telekom</div>
+    <BrowserRouter>
+      <header className='header'>
+        <Banner />
+        <MainNavigation />
+      </header>
+      <main className='main'>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/newRecipe' element={<NewRecipePage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </main>
+      <footer className='footer'>Footer</footer>
+    </BrowserRouter>
   )
 }
---
+++ telekom-learn-react/src/components/MainNavigation.tsx
@@ -0,0 +1,8 @@
+import './MainNavigation.css'
+
+export const MainNavigation = () => {
+  return (<ul className="main-navigation">
+      <li>Homepage</li>
+      <li>Neues Rezept anlegen</li>
+    </ul>)
+}
