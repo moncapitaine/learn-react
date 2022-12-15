d27d2f14e362b864661c8c09faa325d827eca677
Michael Vogt <michael.vogt@progwise.net>
2022-12-15 09:56:34 +0100

    mui

--- telekom-learn-react/src/components/Banner.tsx
+++ telekom-learn-react/src/components/Banner.tsx
@@ -1,9 +1,10 @@
 import './Banner.css'
+import Typography from '@mui/material/Typography'
 
 export const Banner = () => {
   return (<div className='banner'>
     <img width="50px" height="50px" src='/logo.svg' alt="logo" />
-      <span>Kochrezept-Sammlung</span>
+      <Typography variant="h6" component='h1' >Kochrezept-Sammlung</Typography>
     </div>
   )
 }
--- telekom-learn-react/src/components/MainNavigation.tsx
+++ telekom-learn-react/src/components/MainNavigation.tsx
@@ -2,12 +2,13 @@ import { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { UserContext } from '../context/userContext'
 import './MainNavigation.css'
+import Button from '@mui/material/Button'
 
 export const MainNavigation = () => {
   const { userName, setUserName } = useContext(UserContext)
   return (<ul className="main-navigation">
-      <li><Link to="/">Homepage</Link></li>
-      <li><Link to="/newRecipe">Neues Rezept anlegen</Link></li>
-      <li><button onClick={() => setUserName(userName ? undefined : 'Michael')}>{userName ? 'logout' : 'login'}</button></li>
+      <li><Button component={Link}to="/">Homepage</Button></li>
+      <li><Button component={Link} to="/newRecipe">Neu</Button></li>
+      <li><Button onClick={() => setUserName(userName ? undefined : 'Michael')}>{userName ? 'logout' : 'login'}</Button></li>
     </ul>)
 }
